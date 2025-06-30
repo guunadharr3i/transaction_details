75545ccd617121949f593049e122023968021ed5
guunadharr3i <guunadharr.neelikottaam@3i-infotech.com>
2025-06-30 15:27:53 +0530

    changes in pom.xml file and created folder in src

---
+++ src/main/java/com/trasactionData/TransactionDetailsApplication.java
@@ -0,0 +1,11 @@
+package com.trasactionData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionDetailsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionDetailsApplication.class, args);
+    }
+}
