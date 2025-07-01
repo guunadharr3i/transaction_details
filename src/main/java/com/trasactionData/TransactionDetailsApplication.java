d617811622dbfdf8010d7cfc4e539989a4c3bf59
guunadharr3i <guunadharr.neelikottaam@3i-infotech.com>
2025-07-01 13:05:34 +0530

    changed pom file and java file

--- src/main/java/com/trasactionData/TransactionDetailsApplication.java
+++ src/main/java/com/trasactionData/TransactionDetailsApplication.java
@@ -2,9 +2,10 @@ package com.trasactionData;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TransactionDetailsApplication {
+public class TransactionDetailsApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(TransactionDetailsApplication.class, args);
     }
